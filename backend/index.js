67a604dcfadbf8ed47ff61c777c316e0f4ee2fd4
Wilmer Alexander <wilmerh701@fb.com>
2022-11-14 01:13:00 -0500

    [LYL][Backend] Returned index.html file for home url

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const port = 3000
 
+app.use(express.static('/web'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '../web/index.html'))
 })
 
 app.listen(port)
