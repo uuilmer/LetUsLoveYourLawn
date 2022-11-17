dcc4bc9088603e422a7b2fc2df3d97dd2e8a3365
Wilmer Alexander <wilmerh701@fb.com>
2022-11-17 18:41:13 -0500

    [LYL] Refactored various strings

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 3000
+const GENERATED_DIRECTORY = path.join(__dirname, "./generated");
 
-app.use(express.static(path.join(__dirname, "./generated")));
+app.use(express.static(GENERATED_DIRECTORY));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './generated/index.html'))
+    res.sendFile(path.join(GENERATED_DIRECTORY, './index.html'))
 })
 
 app.listen(port)
--- web/index.js
+++ web/index.js
@@ -4,5 +4,5 @@ import LYLContainer from "./components/LYLContainer.js";
 
 ReactDOM.render(
   <LYLContainer />,
-  document.getElementById("page_container")
+  document.getElementById("root")
 );
