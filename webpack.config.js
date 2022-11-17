b1ade708845223bde4e98efc5352a953c4e24811
Wilmer Alexander <wilmerh701@fb.com>
2022-11-17 18:34:03 -0500

    [LYL][Setup][3/N]

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,8 @@
 const path = require('path');
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const port = process.env.PORT || 3000;
 
 module.exports = {
    entry: './web/index.js',
-   target: "web",
    output: {
       path: path.join(__dirname, '/backend/generated'),
       filename: 'index_bundle.js'
